76a696135332d4cc5c017584bf4fbf759ecbdca4
Hossen <mdmitho199911@gmail.com>
2022-03-22 23:15:59 +0600

    my e-commerce website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Header/Shop/Shop';
+
+
+function App() {
+  return (
+    <div className="">
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
+}
+
+
+export default App;
