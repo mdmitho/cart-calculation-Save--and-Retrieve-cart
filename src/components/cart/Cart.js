bbb5a97eb9a2980b6d80d26a447ea5d5261bc52b
Hossen <mdmitho199911@gmail.com>
2022-03-23 13:58:12 +0600

    calculate shopping cart price and tax

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import './Cart.css'
+ 
 const Cart = ({cart}) => {
+    console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    
+    for (const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    
+    }
+    const tax = (total *0.1).toFixed(2)
     return (
-        <div>
+        <div className='cart'>
            <h1>Order Summary</h1>
            <p>Selected Items : {cart.length}</p>
+           <p>Total Price : ${total}</p>
+           <p>Total Shipping : ${shipping}</p>
+           <p>Tax : ${tax}</p>
+           <h4>Grand Total : </h4>
         </div>
     );
 };
