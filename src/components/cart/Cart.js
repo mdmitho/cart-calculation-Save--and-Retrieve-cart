99d185f1326324e56a8ee893efbdad37fded9db7
Hossen <mdmitho199911@gmail.com>
2022-03-23 14:21:43 +0600

    cart data on the localStorage

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -11,7 +11,8 @@ const Cart = ({cart}) => {
         shipping = shipping + product.shipping;
     
     }
-    const tax = (total *0.1).toFixed(2)
+    const tax =parseFloat((total *0.1).toFixed(2)) 
+    const grandTotal = total + shipping + tax
     return (
         <div className='cart'>
            <h1>Order Summary</h1>
@@ -19,7 +20,7 @@ const Cart = ({cart}) => {
            <p>Total Price : ${total}</p>
            <p>Total Shipping : ${shipping}</p>
            <p>Tax : ${tax}</p>
-           <h4>Grand Total : </h4>
+           <h4>Grand Total : ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
